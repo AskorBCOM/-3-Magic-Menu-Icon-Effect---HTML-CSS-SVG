03479fe8ac8cded2ccccfed3371c21ce78a9b4b0
Abraham K <abrahamk@outlook.com>
2014-10-14 20:33:57 +0530

    Final Commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+  // Handler for .ready() called.
+  $header = $('.header__fake');
+
+  $(window).scroll(function() {
+
+      var scroll = $(window).scrollTop();
+
+      if (scroll > 20) {
+          $header.addClass('animated').removeClass('fix');
+      } else {
+          $header.removeClass('animated').addClass('fix');
+      }
+    
+  });
+});
